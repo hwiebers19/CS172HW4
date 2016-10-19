e65fa6d9e6d3fcbf80a0b69c86e6ee290a2925ca
Heidi Wiebers <hwiebers19@hotmail.com>
2016-10-19 14:09:38 -0700

    start homework

---
+++ HW4-CS172/HW4-CS172/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HW4-CS172
+//
+//  Created by Heidi Wiebers on 10/19/16.
+//  Copyright © 2016 Heidi Wiebers. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    
+    return 0;
+}
