603c819dac8d86fa2b513b3b0a731c615917169c
Heidi Wiebers <hwiebers19@hotmail.com>
2016-10-19 14:40:24 -0700

    11.1

--- HW4-CS172/HW4-CS172/main.cpp
+++ HW4-CS172/HW4-CS172/main.cpp
@@ -7,8 +7,37 @@
 //
 
 #include <iostream>
+using namespace std;
 
+// EX04_01 EXERCIZE 11.1
 int main() {
+    int size=0;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+    
+  
+    double array[size];
+    double sum =0;
+    
+    for (int i=0; i < size; i++)
+    {
+        cout << "Enter numbers for the array: ";
+        cin >> array[i];
+        sum += array[i];
+    }
+    
+    double average = sum/size;
+    
+    int count =0;
+    for (int i =0; i < size; i++)
+    {
+        if (array[i] > average)
+            count++;
+    }
+    cout << "The average is: "<<average<<endl;
+    cout << "Number of elements above the average is: "<<count <<endl;
+    
+    
     
     return 0;
 }
